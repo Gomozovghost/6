4b64068de9d40037673e3f39e8b765aedf52c805
Gomozovghost <116781681+Gomozovghost@users.noreply.github.com>
2022-11-22 20:07:09 +0700

    Update index.js

--- 6/index.js
+++ 6/index.js
@@ -1,5 +1,5 @@
 let cikle = [false, false, false, true, false, false, false, false, false, false]
 let dmg = false
-for (let i = 0, x = 0; i < cikle.length; ++i) {if (cikle[i]) {if (dmg) {console.log(`Танк подорвался на mine ${i+1}`); break}
+for (let i = 0, x = 0; i < cikle.length; ++i) {if (cikle[i]) {if (dmg) {console.log(`Танк подорвался на мине ${i+1}`); break}
 else{dmg = true; console.log(`Танк переместился на позицию! Танк поврежден ${i+1}`)}}
 else{console.log(`Танк переместился на позицию ${i+1}`)}} 
